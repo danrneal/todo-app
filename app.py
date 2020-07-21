2ec7a9aaf24f82009ca2e7329958e4e958a05617
Daniel R. Neal <dan.r.neal@gmail.com>
2020-07-21 12:18:29 -0700

    style: Favor double quotes over single

--- app.py
+++ app.py
@@ -24,17 +24,15 @@ from flask import (
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-DIALECT = 'postgresql'
-USER = 'dneal'
-PASS = ''
-HOST = 'localhost'
+DIALECT = "postgresql"
+HOST = "localhost"
 PORT = 5432
-DATABASE = 'todoapp'
+DATABASE = "todo_app"
 
 app = Flask(__name__)
-SQLALCHEMY_DATABASE_URI = f'{DIALECT}://{USER}:{PASS}@{HOST}:{PORT}/{DATABASE}'
-app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+SQLALCHEMY_DATABASE_URI = f"{DIALECT}://{HOST}:{PORT}/{DATABASE}"
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 
@@ -49,14 +47,13 @@ class Todo(db.Model):
         description: A str representing the todo object's content
         list_id: A foreign key linking a todo item to a todo list
     """
-    __tablename__ = 'todos'
+
+    __tablename__ = "todos"
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
     completed = db.Column(db.Boolean, nullable=False, default=False)
     list_id = db.Column(
-        db.Integer,
-        db.ForeignKey('todo_lists.id'),
-        nullable=False
+        db.Integer, db.ForeignKey("todo_lists.id"), nullable=False
     )
 
     def __repr__(self):
@@ -71,18 +68,16 @@ class TodoList(db.Model):
         name: A str representing the todo list object's name
         todos: A relationship for a foreign key on the todo item model
     """
-    __tablename__ = 'todo_lists'
+
+    __tablename__ = "todo_lists"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(), nullable=False)
     todos = db.relationship(
-        'Todo',
-        backref='list',
-        cascade="all, delete-orphan",
-        lazy=True
+        "Todo", backref="list", cascade="all, delete-orphan", lazy=True
     )
 
 
-@app.route('/todos/create', methods=['POST'])
+@app.route("/todos/create", methods=["POST"])
 def create_todo():
     """The route handler for handling post request from users submitting the
     form to create a new todo item
@@ -94,8 +89,8 @@ def create_todo():
     error = False
 
     try:
-        description = request.form.get('description')
-        list_id = request.form.get('list_id')
+        description = request.form.get("description")
+        list_id = request.form.get("list_id")
         todo = Todo(description=description, list_id=list_id)
         db.session.add(todo)
         db.session.commit()
@@ -109,10 +104,10 @@ def create_todo():
     if error:
         abort(500)
 
-    return redirect(url_for('get_list', list_id=list_id))
+    return redirect(url_for("get_list", list_id=list_id))
 
 
-@app.route('/todos/<todo_id>/edit', methods=['POST'])
+@app.route("/todos/<todo_id>/edit", methods=["POST"])
 def set_completed_todo(todo_id):
     """The route handler for handling post request from users checking or
     unchecking a todo item
@@ -127,11 +122,11 @@ def set_completed_todo(todo_id):
     error = False
 
     try:
-        completed = request.get_json()['completed']
+        completed = request.get_json()["completed"]
         todo = Todo.query.get(todo_id)
         todo.completed = completed
         db.session.commit()
-        response = {'success': True}
+        response = {"success": True}
     except Exception:  # pylint: disable=broad-except
         error = True
         db.session.rollback()
@@ -145,7 +140,7 @@ def set_completed_todo(todo_id):
     return jsonify(response)
 
 
-@app.route('/todos/<todo_id>', methods=['DELETE'])
+@app.route("/todos/<todo_id>", methods=["DELETE"])
 def delete_todo(todo_id):
     """The route handler for handling a delete request from users clicking
     the x butten next to the todo item
@@ -163,7 +158,7 @@ def delete_todo(todo_id):
         todo = Todo.query.get(todo_id)
         db.session.delete(todo)
         db.session.commit()
-        response = {'success': True}
+        response = {"success": True}
     except Exception:  # pylint: disable=broad-except
         error = True
         db.session.rollback()
@@ -177,7 +172,7 @@ def delete_todo(todo_id):
     return jsonify(response)
 
 
-@app.route('/lists/create', methods=['POST'])
+@app.route("/lists/create", methods=["POST"])
 def create_list():
     """The route handler responsible to creating a list
 
@@ -188,7 +183,7 @@ def create_list():
     error = False
 
     try:
-        name = request.form.get('name')
+        name = request.form.get("name")
         todo_list = TodoList(name=name)
         db.session.add(todo_list)
         db.session.commit()
@@ -203,10 +198,10 @@ def create_list():
     if error:
         abort(500)
 
-    return redirect(url_for('get_list', list_id=list_id))
+    return redirect(url_for("get_list", list_id=list_id))
 
 
-@app.route('/lists/<list_id>/edit', methods=['POST'])
+@app.route("/lists/<list_id>/edit", methods=["POST"])
 def set_completed_list(list_id):
     """The route handler for handling post request from users clicking complete
     all todos on a list
@@ -226,7 +221,7 @@ def set_completed_list(list_id):
         for todo in todo_list.todos:
             todo.completed = True
         db.session.commit()
-        response = {'success': True}
+        response = {"success": True}
     except Exception:  # pylint: disable=broad-except
         error = True
         db.session.rollback()
@@ -240,7 +235,7 @@ def set_completed_list(list_id):
     return jsonify(response)
 
 
-@app.route('/lists/<list_id>', methods=['DELETE'])
+@app.route("/lists/<list_id>", methods=["DELETE"])
 def delete_list(list_id):
     """The route handler for handling a delete request from users clicking
     the x butten next to the todo list
@@ -258,7 +253,7 @@ def delete_list(list_id):
         todo_list = TodoList.query.get(list_id)
         db.session.delete(todo_list)
         db.session.commit()
-        response = {'success': True}
+        response = {"success": True}
     except Exception:  # pylint: disable=broad-except
         error = True
         db.session.rollback()
@@ -272,7 +267,7 @@ def delete_list(list_id):
     return jsonify(response)
 
 
-@app.route('/lists/<list_id>')
+@app.route("/lists/<list_id>")
 def get_list(list_id):
     """The route handler for a list's page
 
@@ -284,16 +279,14 @@ def get_list(list_id):
     """
     lists = TodoList.query.all()
     active_list = TodoList.query.get(list_id)
-    todos = Todo.query.filter_by(list_id=list_id).order_by('id').all()
+    todos = Todo.query.filter_by(list_id=list_id).order_by("id").all()
+
     return render_template(
-        'index.html',
-        lists=lists,
-        active_list=active_list,
-        todos=todos
+        "index.html", lists=lists, active_list=active_list, todos=todos
     )
 
 
-@app.route('/')
+@app.route("/")
 def index():
     """The route handler for the homepage
 
@@ -301,8 +294,8 @@ def index():
         A redirect to the first list in the db
     """
     first_list = TodoList.query.first()
-    return redirect(url_for('get_list', list_id=first_list.id))
+    return redirect(url_for("get_list", list_id=first_list.id))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
